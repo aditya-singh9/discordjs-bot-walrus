3cc9e1609f7befbb9c22ddbf5ddab2346a613d64
Aditya Singh <75490282+aditya0930@users.noreply.github.com>
2021-02-26 23:51:23 +0530

    added music feature

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,14 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const { Client, WebhookClient } = require("discord.js");
 const command = require("./command");
+const ytdl = require("ytdl-core");
+const { YTSearcher } = require("ytsearcher");
+const queue = new Map();
 
+const searcher = new YTSearcher({
+  key: "AIzaSyAhvLottGirD7PNhmEdcp1UDeIMyVI2zN8",
+  revealed: true,
+});
 const client = new Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"], //({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]})
 });
@@ -53,6 +60,7 @@ client.on("ready", () => {
 });
 
 client.on("message", async (message) => {
+  const serverQueue = queue.get(message.guild.id);
   if (message.author.bot) return;
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
@@ -88,6 +96,88 @@ client.on("message", async (message) => {
     } else if (CMD_NAME === "announce") {
       const msg = args.join(" ");
       webhookClient.send(msg);
+    }
+
+    //music feature
+    switch (CMD_NAME) {
+      case "play":
+        execute(message, serverQueue);
+        break;
+      case "stop":
+        stop(message, serverQueue);
+        break;
+      case "skip":
+        skip(message, serverQueue);
+        break;
+    }
+    async function execute(message, serverQueue) {
+      let vc = message.member.voice.channel;
+      if (!vc) {
+        return message.channel.send("Please join a voice chat first");
+      } else {
+        let result = await searcher.search(args.join(" "), { type: "video" });
+        const songInfo = await ytdl.getInfo(result.first.url);
+
+        let song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+        };
+
+        if (!serverQueue) {
+          const queueConstructor = {
+            txtChannel: message.channel,
+            vChannel: vc,
+            connection: null,
+            songs: [],
+            volume: 10,
+            playing: true,
+          };
+          queue.set(message.guild.id, queueConstructor);
+
+          queueConstructor.songs.push(song);
+
+          try {
+            let connection = await vc.join();
+            queueConstructor.connection = connection;
+            play(message.guild, queueConstructor.songs[0]);
+          } catch (err) {
+            console.error(err);
+            queue.delete(message.guild.id);
+            return message.channel.send(`Unable to join the voice chat ${err}`);
+          }
+        } else {
+          serverQueue.songs.push(song);
+          return message.channel.send(`The song has been added ${song.url}`);
+        }
+      }
+    }
+    function play(guild, song) {
+      const serverQueue = queue.get(guild.id);
+      if (!song) {
+        serverQueue.vChannel.leave();
+        queue.delete(guild.id);
+        return;
+      }
+      const dispatcher = serverQueue.connection
+        .play(ytdl(song.url))
+        .on("finish", () => {
+          serverQueue.songs.shift();
+          play(guild, serverQueue.songs[0]);
+        });
+      serverQueue.txtChannel.send(`Now playing ${serverQueue.songs[0].url}`);
+    }
+    function stop(message, serverQueue) {
+      if (!message.member.voice.channel)
+        return message.channel.send("You need to join the voice chat first!");
+      serverQueue.songs = [];
+      serverQueue.connection.dispatcher.end();
+    }
+    function skip(message, serverQueue) {
+      if (!message.member.voice.channel)
+        return message.channel.send("You need to join the voice chat first");
+      if (!serverQueue)
+        return message.channel.send("There is nothing to skip!");
+      serverQueue.connection.dispatcher.end();
     }
   }
 });
